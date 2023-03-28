d09105167a0695de727e1f8de162f1ee0380bcce
Gu111aum3 <gderouin@ymail.com>
2023-03-28 09:22:09 +0200

    test map

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -9,25 +9,31 @@ btnMainMenu.addEventListener("click", function () {
 });
 /*                                               Google maps                                        */
 
-// import { Loader } from "@googlemaps/js-api-loader"
 
-
-
-// const loader = new Loader({
-//   apiKey: "AIzaSyCWh921gdpP63AAGNR6wB9ZENUwOCF7nPU",
-//   version: "weekly",
-//   ...additionalOptions,
-// });
-
-// loader.load().then(() => {
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: { lat: -34.397, lng: 150.644 },
-//     zoom: 10,
-//   });
-// });
 
 let map;
 const haubourdin = { lat: 50.611784712322354, lng: 2.975147782208914 };
+
+const contentString = '<div id="content">' +
+'<div id="siteNotice">' +
+"</div>" +
+'<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+'<div id="bodyContent">' +
+"<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+"sandstone rock formation in the southern part of the " +
+"Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+"south west of the nearest large town, Alice Springs; 450&#160;km " +
+"(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+"features of the Uluru - Kata Tjuta National Park. Uluru is " +
+"sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+"Aboriginal people of the area. It has many springs, waterholes, " +
+"rock caves and ancient paintings. Uluru is listed as a World " +
+"Heritage Site.</p>" +
+'<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+"https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+"(last visited June 22, 2009).</p>" +
+"</div>" +
+"</div>";
 
 function initMap() {
    map = new google.maps.Map(document.getElementById("map"), {
@@ -35,15 +41,16 @@ function initMap() {
     zoom: 15,
   });
 
-  const maker = new google.maps.Marker(
+  const marker = new google.maps.Marker(
     {
         position: haubourdin,
         map: map,
-
+        title: "Hello World",
     }
   )
   
 }
 
 window.initMap = initMap;
+marker.setMap(map);
 
