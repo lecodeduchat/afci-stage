a477da7a3bfea454cfb1988baef12f937184d648
Gu111aum3 <gderouin@ymail.com>
2023-05-26 12:01:54 +0200

    ajout captcha

--- public/assets/js/recaptcha.js
+++ public/assets/js/recaptcha.js
@@ -1,7 +1,7 @@
 function onClick(e) {
     e.preventDefault();
     grecaptcha.ready(function() {
-      grecaptcha.execute('6Les4D8mAAAAAN024FwHssFg2v3DLX1i3oHtoa2O', {action: 'submit'}).then(function(token) {
+      grecaptcha.execute('cle captcha', {action: 'submit'}).then(function(token) {
           // Add your logic to submit to your backend server here.
       });
     });
