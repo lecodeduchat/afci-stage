3a6d7eabf2520b0a2dd13a4798262916fb500297
Gu111aum3 <gderouin@ymail.com>
2023-06-15 11:28:13 +0200

    plus de clef captcha

--- public/assets/js/recaptcha.js
+++ public/assets/js/recaptcha.js
@@ -1,20 +1,14 @@
 // cle du site
-let cleCaptcha = "6Ley7lsmAAAAABFlnMA7qvP806dBQ50ZeOtmA84b";
+let cleCaptcha = "";
 
 function onClick(e) {
-<<<<<<< HEAD
     e.preventDefault();
     grecaptcha.ready(function() {
-      grecaptcha.execute('6LfhAlwmAAAAABkY8ssrGY_uLMmnDa5R7I8Cosq8', {action: 'submit'}).then(function(token) {
+      grecaptcha.execute('', {action: 'submit'}).then(function(token) {
           // Add your logic to submit to your backend server here.
-=======
-  e.preventDefault();
-  grecaptcha.ready(function () {
-    grecaptcha
-      .execute("6LfhAlwmAAAAABkY8ssrGY_uLMmnDa5R7I8Cosq8", { action: "submit" })
-      .then(function (token) {
-        // Add your logic to submit to your backend server here.
->>>>>>> main
+
+
+
       });
   });
 }
